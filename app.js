eb49c1b60acfd03b933dbb532b7ba1e286929796
d4rcel <daraola05@gmail.com>
2023-05-06 17:54:08 +0100

    Update task functionnality

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks.routes");
-const connectDB = require('./db/connect')
-require('dotenv').config()
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 // middleware
+app.use(express.static("./public"));
 app.use(express.json());
 
 // routes
-app.get("/hello", (req, res) => {
-  res.send("Task Manager App");
-});
-
 app.use("/api/v1/tasks", tasks);
 
 // app.get('api/v1/tasks')
@@ -20,11 +17,11 @@ const port = 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI)
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`App listening on port ${port}...`));
   } catch (error) {
     console.log(error);
   }
-}
+};
 
-start()
+start();
